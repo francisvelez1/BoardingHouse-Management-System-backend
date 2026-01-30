728e49ebed7273e30fe2b895dce2e3a77eb932f9
brylle60 <johnbryllecrodua77@gmail.com>
2026-01-30 16:26:41 +0800

    initial commit

---
+++ src/test/java/com/BoardingHouse/BoardingHouse_Management_System/BoardingHouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BoardingHouse.BoardingHouse_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingHouseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
