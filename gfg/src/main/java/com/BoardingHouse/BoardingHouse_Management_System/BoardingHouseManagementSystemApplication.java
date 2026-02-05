719733054f10650ac891f6b03f3358aca0315861
francisvelez1 <francis.velez231@gmail.com>
2026-02-05 21:30:16 +0800

    initial push

--- gfg/src/main/java/com/BoardingHouse/BoardingHouse_Management_System/BoardingHouseManagementSystemApplication.java
+++ gfg/src/main/java/com/BoardingHouse/BoardingHouse_Management_System/BoardingHouseManagementSystemApplication.java
@@ -8,6 +8,7 @@ public class BoardingHouseManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardingHouseManagementSystemApplication.class, args);
+		System.out.println(
+				"Welcome to the Boarding House Management System! This application helps you manage your boarding house efficiently. You can add tenants, manage rooms, and keep track of payments all in one place. Let's get started!");
 	}
-
 }
